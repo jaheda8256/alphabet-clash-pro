0cea7217c36d45ab4d570194407a3ca07c9cb584
jaheda8256 <jahedasultana47@gmail.com>
2024-02-16 19:40:37 +0600

    added new game

--- scripts/alpha.clash.js
+++ scripts/alpha.clash.js
@@ -9,8 +9,20 @@
 // playGroundSection.classList.remove('hidden');
 // // console.log(playGroundSection.classList);
 //  }
+function continueGame(){
+    // generate a random alphabet 
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
+    // set randomly generated alphabet to the screen(show it
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+    // set backgroundColor
+    setBackgroundColorById(alphabet);
+
+}
 
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
